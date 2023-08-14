10573d988bf1a45db8aac12e19ca6f2c162a323a
RkbotzTG <140271142+RkbotzTG@users.noreply.github.com>
2023-08-14 23:50:54 +0530

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -27,7 +27,7 @@ STRING = os.environ.get("STRING", "")
 app = Client("test", api_id=API_ID, api_hash=API_HASH, session_string=STRING)
 
 # Add the URL of the default thumbnail
-default_thumbnail_url = "https://telegra.ph/file/f2b805cc65089bc72d153.jpg"
+#default_thumbnail_url = "https://telegra.ph/file/f2b805cc65089bc72d153.jpg"
 
 @Client.on_callback_query(filters.regex('cancel'))
 async def cancel(bot,update):
