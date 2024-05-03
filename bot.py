7aed7548ec02ff16eee0675467dad7a9bc849a38
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-05-03 21:35:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,20 @@
 import asyncio
 from pyrogram import Client, compose,idle
 import os
-
 from plugins.cb_data import app as Client2
 
+API_ID = int(os.environ.get("API_ID", "24998279"))
+API_HASH = os.environ.get("API_HASH", "b2ec3ab8ed156e7a6782f3d7d1acbaf6")
 TOKEN = os.environ.get("TOKEN", "6525627371:AAEPgQ_2jxzzrEv4VHrfRAc1IhBrBNbKH7I")
 
-API_ID = int(os.environ.get("API_ID", "24998279"))
-
-API_HASH = os.environ.get("API_HASH", "b2ec3ab8ed156e7a6782f3d7d1acbaf6")
-
 STRING = os.environ.get("STRING", "")
-
 
 bot = Client(
 
            "Rename",
-
            bot_token=TOKEN,
-
            api_id=API_ID,
-
            api_hash=API_HASH,
-
            plugins=dict(root='plugins'))
            
 
