8e5053510792e40bf6e9eb79ff37952229f364de
technical_aks <105633032+technicalaks@users.noreply.github.com>
2024-05-04 10:21:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,15 @@
 import asyncio
 from pyrogram import Client, compose,idle
 import os
-
 from plugins.cb_data import app as Client2
 
 TOKEN = os.environ.get("TOKEN", "6525627371:AAEPgQ_2jxzzrEv4VHrfRAc1IhBrBNbKH7I")
-
 API_ID = int(os.environ.get("API_ID", "24998279"))
-
 API_HASH = os.environ.get("API_HASH", "b2ec3ab8ed156e7a6782f3d7d1acbaf6")
+DB_NAME = os.environ.get("DB_NAME", "Raj")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://Jng:jng@cluster0.vvrnobg.mongodb.net/?retryWrites=true&w=majority")
 
 STRING = os.environ.get("STRING", "")
-
 
 bot = Client(
 
